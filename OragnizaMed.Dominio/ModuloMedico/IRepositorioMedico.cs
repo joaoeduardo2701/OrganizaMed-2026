10f02ec17deb31d646aef712d043e04c991a6376
João Eduardo Cardoso Padilha <duducardoso2701@gmail.com>
2026-03-02 18:49:28 -0300

    Repositorio Medico Adicionado

--- OragnizaMed.Dominio/ModuloMedico/IRepositorioMedico.cs
+++ OragnizaMed.Dominio/ModuloMedico/IRepositorioMedico.cs
@@ -2,5 +2,11 @@
 {
     public interface IRepositorioMedico
     {
+        Task<Guid> InserirAsync(Medico medico);
+        Task<bool> EditarAsync(Medico medico);
+        Task<bool> ExcluirAsync(Medico medico);
+        Task<List<Medico>> SelecionarTodosAsync(Guid id); 
+        Task<Medico?> SelecionarPorIdAsync(Guid id);
+        Task<List<Medico>> SelecionarMuitosPorId(IEnumerable<Guid> requestMedicos);
     }
 }
