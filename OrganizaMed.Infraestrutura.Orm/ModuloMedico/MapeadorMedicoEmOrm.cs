87c0cdc6ba0e34d33c9cc74688cb6b69699528d5
João Eduardo Cardoso Padilha <duducardoso2701@gmail.com>
2026-03-15 00:41:05 -0300

    Swagger adicionado

---
+++ OrganizaMed.Infraestrutura.Orm/ModuloMedico/MapeadorMedicoEmOrm.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using OrganizaMed.Dominio.ModuloMedico;
+
+namespace OrganizaMed.Infraestrutura.Orm.ModuloMedico
+{
+    public class MapeadorMedicoEmOrm : IEntityTypeConfiguration<Medico>
+    {
+        public void Configure(EntityTypeBuilder<Medico> builder)
+        {
+            builder.ToTable("TBMedico");
+            
+            builder.Property(m => m.Id)
+                .ValueGeneratedNever();
+
+            builder.Property(m => m.Nome)
+                .HasColumnType("nvarchar(100)")
+                .IsRequired();
+
+            builder.Property(m => m.Crm)
+                .HasColumnType("char(8)")
+                .IsRequired();        
+        }
+    }
+}
